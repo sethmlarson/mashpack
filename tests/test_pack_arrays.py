129cad20156047aa474238cebbe6af24c651766b
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2018-01-17 11:26:42 -0600

    Add unit tests for marray-array repacking

---
+++ tests/test_pack_arrays.py
@@ -0,0 +1,32 @@
+import pytest
+import mashpack
+
+@pytest.mark.parametrize(
+    ['array', 'expected'],
+    [([], b'\x80'),
+     ([1], b'\x81\xA1'),
+     ([1, 2], b'\x82\xA1\xA2'),
+     ([1, 2, 3], b'\x83\xA1\xA2\xA3'),
+     ([1, 2, 3, 4], b'\x84\xA1\xA2\xA3\xA4')]
+)
+def test_short_array_always_marrayp(array, expected):
+    packed = mashpack.packb(array)
+    assert packed == expected
+    
+    
+def test_typed_marrayp_requires_4_elements_to_repack_to_array8():
+    array = [255] * 3
+    packed = mashpack.packb(array)
+    assert packed == b'\x84\xF1\xFF\xF1\xFF\xF1\xFF'
+
+    array = [255] * 4
+    packed = mashpack.packb(array)
+    assert packed == b'\xE8\x05\xF1\xFF\xFF\xFF\xFF'
+
+@pytest.mark.parametrize(
+    ['array', 'expected'],
+    [([255] * 64, b'\xE8\x40\xF1' + ('\xFF' * 64))]
+)
+def test_typed_marrayp_packed_to_array8(array, expected):
+    packed = mashpack.packb(array)
+    assert packed == expected
