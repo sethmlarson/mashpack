90c021788b5d97b668aba5579db7f41497be09ab
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2018-01-17 12:41:23 -0600

    Add test that objects don't change width to convert to ARRAY8

--- tests/test_pack_arrays.py
+++ tests/test_pack_arrays.py
@@ -14,19 +14,28 @@ def test_short_array_always_marrayp(array, expected):
     assert packed == expected
     
     
-def test_typed_marrayp_requires_4_elements_to_repack_to_array8():
+def test_typed_marrayp_requires_3_elements_to_repack_to_array8():
+    array = [255] * 2
+    packed = mashpack.packb(array)
+    assert packed == b'\x84\xF1\xFF\xF1\xFF'
+
     array = [255] * 3
     packed = mashpack.packb(array)
-    assert packed == b'\x84\xF1\xFF\xF1\xFF\xF1\xFF'
-
-    array = [255] * 4
-    packed = mashpack.packb(array)
-    assert packed == b'\xE8\x05\xF1\xFF\xFF\xFF\xFF'
+    assert packed == b'\xE8\x05\xF1\xFF\xFF\xFF'
 
 @pytest.mark.parametrize(
     ['array', 'expected'],
     [([255] * 64, b'\xE8\x40\xF1' + ('\xFF' * 64))]
 )
 def test_typed_marrayp_packed_to_array8(array, expected):
+    packed = mashpack.packb(array)
+    assert packed == expected
+
+    
+@pytest.mark.parametrize(
+    ['array', 'expected'],
+    [([0xFF, 0xFFFF, 1], b'\x83\xF1\xFF\xF2\xFF\xFF\xA1')]
+)
+def test_typed_marrayp_no_change_obj_width(array, expected):
     packed = mashpack.packb(array)
     assert packed == expected
